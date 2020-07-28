b55f935331719cc693958d830daba4d088afcb01
MartinJoy <mrtnrada@gmail.com>
2020-07-28 19:37:06 +0200

    version 1.2

--- src/com/rada/engeto/mergesort/Main.java
+++ src/com/rada/engeto/mergesort/Main.java
@@ -1,7 +1,5 @@
 package com.rada.engeto.mergesort;
-
-import java.io.BufferedReader;
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
@@ -10,9 +8,20 @@ public class Main {
         Integer[] a = {8,1,7,5,4,2,3,9,6};
         String[] b = {"h","d","g","e","i","a","b","f","c"};
 
+        // Generate random numbers for test
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt() & Integer.MAX_VALUE;
+        }
+
         MergeSort<Integer> mergeSortInteger = new MergeSort<>();
         mergeSortInteger.run(a);
         System.out.println(java.util.Arrays.toString(a));
+
+        MergeSort<Integer> mergeSortRandom = new MergeSort<>();
+        mergeSortRandom.run(arr);
+        System.out.println(java.util.Arrays.toString(arr));
 
         MergeSort<String> mergeSortString = new MergeSort<>();
         mergeSortString.run(b);
